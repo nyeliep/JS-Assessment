d5d9112a3bca2a9eeff5f00c237f059dd696bd48
nyeliep <gielpalnyeliep@gmail.com>
2023-04-11 09:07:59 +0300

    commit

---
+++ assessment.js
@@ -0,0 +1,78 @@
+// 1. Write a function that takes in a string and returns it when reversed
+// let food = “eating”
+function reversedStr(str){
+  return str.reverse()
+}
+let food = "eating"
+let reversedFood = reversedStr(food)
+console.log(reversedFood);
+// 2. Write a function that takes in the following array and consoles the target if it is found else
+// null
+// let num = [2,8,0,23,5,45,76]
+// Target = 23
+function array(num,target){
+  let left = 0;
+  let right = -1;
+  if(left.length===right.length){
+    return num
+  }
+ 
+  let middle = num.Mathfloor(left+right/2)
+  if(num[middle]<right){
+    
+  }
+  else if(num[middle>left]){
+
+  }
+  console.log(array(num,target),...left,...right)
+  
+
+}
+
+function sortedArray(arr){
+  let emptyArr = []
+
+
+}
+// 3. Given years between 2000 and 2023, console all the leap years in the following
+// sentence, i.e “2020 is a leap year” if not console log i.e “2001 is not a leap year”
+function leapYears(years){
+  
+
+}
+// 4. Given a range of numbers from 0 to 100, console”Fizz” if the numbers are divisible by 3,
+// “Buzz” if the numbers are divisible by 5, and “FizzBuzz” if divisible by both 3 and 5.
+function divisibleArr(arr){
+  if(num/15){
+    console.log("fizzBuzz")
+  }
+  else if(num/5){
+    console.log("fizz")
+  }
+  else if(num/3){
+    console.log("Buzz")
+  }
+}
+// 5. Write a function that takes in an array of numbers and returns an array that has all
+// elements multiplied by 4.
+// let numArray = [12,87,45,75,23,64,73]
+function multArry (arr){
+  while(i< arr.length){
+    for(i in arr){
+      return arr*multi
+    i++
+    }
+  }
+}
+let numArray = [12,87,45,75,23,64,73]
+let multi = 4
+let multipliedArray = multArry(numArray)
+console.log(multipliedArray)
+// 6. Write a function that takes in an array of strings and returns an array with every element
+// turned into a number
+// let nums = [“10”,”24”,”45”,”56”,”67”]
+function numbers(arr){
+return arr.toNumbers()
+}
+let nums = ["10","24","45","56","67"]
+console.log(numbers(nums))
